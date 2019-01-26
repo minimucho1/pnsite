0185d8476f93a08fccc2857311990ccfd95a256f
JR <minimucho1@gmail.com>
2019-01-26 01:57:39 -0600

    JR | Added Header component and created HomePage component

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header">
+    <a class="header__logo left" href="#">Project Nano</a>
+    <div class="header__links right">
+      <a class="header__link" href="#">About</a>
+      <a class="header__link" href="#">Download</a>
+      <a class="header__link" href="#">Rules</a>
+      <a class="header__link" href="#">Forums</a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+.header {
+  margin: auto;
+  width: 800px;
+  height: 61px;
+}
+
+.left {
+  position: relative;
+  float: left;
+  left: 10px;
+}
+
+.right {
+  position: relative;
+  float: right;
+  line-height: 61px;
+  right: 10px;
+}
+
+.header__logo {
+  font-size: 40px;
+  border-style: solid;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+
+.header__links {
+  font-size: 28px;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+a:hover {
+  color: blue;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header/>
+    <hr />
+    <!-- <Sidebar /> -->
+    <!-- <Content /> -->
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+
+export default {
+  name: 'Homepage',
+  components: { Header },
+};
+</script>
+
+<style scoped>
+hr {
+  border-style: solid
+}
+</style>
